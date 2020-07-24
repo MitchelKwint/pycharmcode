8fb6c95c1b99ed99d9d456ab33e2d6264ab9c112
Mitchel <Contactmitchyt@gmail.com>
2020-07-24 10:08:54 +0200

    Test

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from Student import Student
 
 student1 = Student("Jim", "Busisness", 3.1,  False)
 student2 = Student("Pam", "Art", 2.5,  True)
+student3 = Student("Ar", "Math", 3.5,  True)
 
 print(student1.name)
